b775538bc407bae7f9ed8db5ba590bff463418c5
NANASE <48847610+nax0100@users.noreply.github.com>
2021-05-05 14:10:37 +0900

    Delete AndroidStudioProjects directory

--- AndroidStudioProjects/doo/android/app/src/main/kotlin/com/example/doo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.doo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- AndroidStudioProjects/topiker/android/app/src/main/kotlin/com/example/topiker/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.topiker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
